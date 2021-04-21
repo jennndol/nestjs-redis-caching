abc1c6c142ae29364bc1e59387468cbe71358b40
Luthfi <jkt.luthfi@gmail.com>
2021-04-21 16:15:26 +0700

    UPDATE readme.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ## SOLVA REDIS CACHING
 
+### Route List
+
+|No | Endpoint | Method  | Description | 
+|---|---|---|---|
+| 1 | /  | GET | Caching with default key '/'  |
+| 2 | /  | POST  | Delete cache that has key '/'|
+| 3 | /custom  | GET | Caching with custom key 'customKey'  |
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,31 @@
-import { CacheKey, Controller, Get } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  CacheKey,
+  Controller,
+  Get,
+  Inject,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cache } from 'cache-manager';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER)
+    private readonly cache: Cache,
+  ) {}
 
   @Get()
   defaultCacheKey(): Promise<string> {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async deleteCacheKey(): Promise<string> {
+    await this.cache.del('/');
+    return 'cache key deleted /';
   }
 
   @Get('/custom')
